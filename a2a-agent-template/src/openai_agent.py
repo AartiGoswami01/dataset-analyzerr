4397887d1352faa9d2c920d2b6cfcdcd868ba62d
AartiGoswami01 <aartigoswami10969@gmail.com>
2026-03-29 14:34:08 +0530

    removed extra folder

--- a2a-agent-template/src/openai_agent.py
+++
@@ -1,9 +0,0 @@
-from agent_toolset import DatasetAnalyzerToolset
-
-def create_agent():
-    toolset = DatasetAnalyzerToolset()
-
-    return {
-        "tools": toolset.get_tools(),
-        "system_prompt": "You are a dataset analyzer. Always return results in JSON format."
-    }
